ef680ba6cdf5aae3e4dce4eaa1e62b138013c468
Edwin Manuel Cerrón Angeles <xerron.angels@gmail.com>
2017-05-26 19:33:47 -0500

    License

---
+++ src/App/src/Action/SiteMaintenanceMode.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xerron
+ * Date: 11/05/17
+ * Time: 11:50 PM
+ */
+
+namespace App\Action;
+
+
+class SiteMaintenanceMode
+{
+    public function __invoke()
+    {
+        echo "Maintenance site";
+    }
+
+}
---
+++ src/App/src/ConfigProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+/**
+ * The configuration provider for the App module
+ *
+ * @see https://docs.zendframework.com/zend-component-installer/
+ */
+class ConfigProvider
+{
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     *
+     * @return array
+     */
+    public function __invoke()
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+            'templates'    => $this->getTemplates(),
+            'routes'    => $this->getRoutes(),
+        ];
+    }
+
+    /**
+     * Returns the container dependencies
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [
+            'services' => [
+
+            ],
+            'shared'  => [
+
+            ],
+        ];
+    }
+
+    /**
+     * Returns the routes
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return [
+        ];
+    }
+    /**
+     * Returns the templates configuration
+     *
+     * @return array
+     */
+    public function getTemplates()
+    {
+        return [
+            'paths' => [
+                'app'    => [__DIR__ . '/../templates/app'],
+                'error'  => [__DIR__ . '/../templates/error'],
+                'layout' => [__DIR__ . '/../templates/layout'],
+            ],
+        ];
+    }
+}
